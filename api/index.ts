58a7ceb9827ac57935c2693513ac3b45ed27ba03
Raj Vora <rajvora@ufl.edu>
2025-02-25 18:11:34 -0500

    health

--- api/index.ts
+++ api/index.ts
@@ -46,9 +46,4 @@ app.use(function (req, res, next) {
     res.status(404).send('Sorry we could not find that.')
 })
 
-// Start express app
-app.listen(3000, function() {
-    console.log(`Server is running on: 3000`)
-  })
-
 module.exports = app;
